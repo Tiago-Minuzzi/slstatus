2dfd81217b3b5e80789e38fe2f622e27e069f028
Tiago-Minuzzi <tiago.minuzzi87@gmail.com>
2021-11-10 17:39:42 -0300

    Weather crontab

--- config.h
+++ config.h
@@ -65,7 +65,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ run_command, " %s | ",     "curl 'wttr.in?format=1'" },
+	{ run_command, " %s | ",     "cat /tmp/currWeather.txt" },
 	{ cpu_perc, " %2s%% | ",     NULL },
 	{ ram_perc, " %2s%% | ",     NULL },
 	{ swap_perc, " %2s%% | ",     NULL },
